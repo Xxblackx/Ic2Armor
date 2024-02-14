2640ea3622a6e218443a6ca5bc302b724c585d60
Xxblackx <86732716+Xxblackx@users.noreply.github.com>
2024-02-14 16:24:41 +0700

    2nd commit test

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -38,7 +38,9 @@ public class ExampleMod
 	// test eclipse commit
     // Define mod id in a common place for everything to reference
     public static final String MODID = "examplemod";
-    // Directly reference a slf4j logger
+    //test push to my dev branch
+	// test eclipse commit
+	// Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
     // Create a Deferred Register to hold Blocks which will all be registered under the "examplemod" namespace
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MODID);
